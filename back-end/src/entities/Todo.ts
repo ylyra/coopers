e35cea002589fd527db23ed93ee84c174b3935e3
Yan Lyra <yanlyra3@gmail.com>
2021-05-26 15:08:08 -0300

    :card_file_box: Created User and Todo Entities

---
+++ back-end/src/entities/Todo.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { User } from "./User";
+
+@Entity("todos")
+class Todo {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  text: string;
+
+  @Column()
+  hasCompleted: boolean;
+
+  @Column()
+  user_id: string;
+
+  @JoinColumn({ name: "user_id" })
+  @ManyToOne(() => User)
+  user: User;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Todo };
---
+++ back-end/src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  token: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
