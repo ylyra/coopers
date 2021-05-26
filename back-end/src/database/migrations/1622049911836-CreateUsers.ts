c5ef144e3c7b6f34cc586e0c7468edc7d82160cb
Yan Lyra <yanlyra3@gmail.com>
2021-05-26 14:45:05 -0300

    :sparkles: Add users and todos tables
    Created backend connection and created users and todos tables.

---
+++ back-end/src/database/migrations/1622049911836-CreateUsers.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1622049911836 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "token",
+            type: "varchar",
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
---
+++ back-end/src/database/migrations/1622050395913-CreateTodos.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTodos1622050395913 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "todos",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "text",
+            type: "varchar",
+          },
+          {
+            name: "hasCompleted",
+            type: "boolean",
+            default: false,
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKTodo",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("todos");
+  }
+}
