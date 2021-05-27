4f0e856e2d4f98eff3e76dec0373bbe742072b05
Yan Lyra <yanlyra3@gmail.com>
2021-05-27 17:18:05 -0300

    :iphone: Created media query to responsive design

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -106,6 +106,8 @@ export default function Home({ isLogged }: IHomeProps) {
   );
   const [todo, setTodo] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function move(
     source: ITodo[],
@@ -201,6 +203,10 @@ export default function Home({ isLogged }: IHomeProps) {
     setIsModalOpen(false);
   }
 
+  function handleLogin(event: FormEvent) {
+    event.preventDefault();
+  }
+
   return (
     <Fragment>
       <Head>
@@ -262,13 +268,14 @@ export default function Home({ isLogged }: IHomeProps) {
 
             <p>The only way to get things done</p>
 
-            <Button
+            <Link
               to="todoList"
-              type="submit"
-              value="Go to To-do list"
+              className={styles.goToTodo}
               smooth={true}
               duration={500}
-            />
+            >
+              Go to To-do list
+            </Link>
           </div>
 
           <Image src="/foto.png" width={443} height={481} />
@@ -410,7 +417,44 @@ export default function Home({ isLogged }: IHomeProps) {
         <div className={`${styles.modal} ${isModalOpen ? styles.active : ""}`}>
           <div className={styles.modalBackground} onClick={handleCloseModal} />
           <div className={styles.modalContent}>
-            <h1>modal</h1>
+            <div>
+              <img src="/login.svg" alt="Login banner" />
+            </div>
+
+            <div>
+              <h1>
+                Sign in
+                <span>to access your list</span>
+              </h1>
+
+              <form onSubmit={handleLogin}>
+                <div>
+                  <label htmlFor="email">E-mail:</label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </div>
+
+                <button type="submit">Sign in</button>
+              </form>
+
+              <button className={styles.closeModal} onClick={handleCloseModal}>
+                close
+              </button>
+            </div>
           </div>
         </div>
       )}
@@ -420,20 +464,29 @@ export default function Home({ isLogged }: IHomeProps) {
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   let isLogged = false;
+  let todos = {};
+  let completedTodos = {};
   const { _coopers_user_token } = ctx.req.cookies;
 
   if (_coopers_user_token) {
-    const response = await api.get("/user/verify", {
-      params: {
-        token: _coopers_user_token,
-      },
-    });
-    isLogged = true;
+    try {
+      await api.get("/user/verify", {
+        params: {
+          token: _coopers_user_token,
+        },
+      });
+      api.defaults.headers.common = {
+        Authorization: `Bearer ${_coopers_user_token}`,
+      };
+      isLogged = true;
+    } catch (err) {}
   }
 
   return {
     props: {
       isLogged,
+      todos,
+      completedTodos,
     },
   };
 };
